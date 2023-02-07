57d31997df9a18869f562ebb965bd15704929043
Rafael de Jesus <rrrrebolledohdz@gmail.com>
2023-02-07 14:32:38 -0600

    add typeOrm, pg, and remove docker, working with railway

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.PGPORT,
+      username: process.env.PGUSER,
+      database: process.env.PGDATABASE,
+      password: process.env.PGPASSWORD,
+      autoLoadEntities: process.env.NODE_ENV === 'development',
+      synchronize: process.env.NODE_ENV === 'development',
+    }),
+  ],
   controllers: [],
   providers: [],
 })

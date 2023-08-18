848151e4bc126e62cd62c2fa560c17f8b636acb8
Rafael De Jesus Rebolledo <>
2023-08-18 14:05:50 -0600

    school module working

--- src/school/school.module.ts
+++ src/school/school.module.ts
@@ -1,19 +1,15 @@
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Module, forwardRef } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { SchoolService } from './school.service';
 import { SchoolController } from './school.controller';
 import { School } from './entities/school.entity';
-import { AuthModule } from 'src/auth/auth.module';
 import { FilesModule } from 'src/files/files.module';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   controllers: [SchoolController],
   providers: [SchoolService],
-  imports: [
-    TypeOrmModule.forFeature([School]),
-    forwardRef(() => AuthModule),
-    FilesModule,
-  ],
+  imports: [TypeOrmModule.forFeature([School]), AuthModule, FilesModule],
   exports: [TypeOrmModule, SchoolService],
 })
 export class SchoolModule {}

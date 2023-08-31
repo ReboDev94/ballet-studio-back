2c3cad2eef0f088999d1f9accd6de41cc76b1be3
Rafael De Jesus Rebolledo <>
2023-08-31 15:51:53 -0600

    return errors with keys property

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { BadRequestException, ValidationPipe } from '@nestjs/common';
 import { config } from 'aws-sdk';
 
 async function bootstrap() {
@@ -12,6 +12,14 @@ async function bootstrap() {
       transform: true,
       whitelist: true,
       forbidNonWhitelisted: true,
+      stopAtFirstError: true,
+      exceptionFactory(errors) {
+        const result = errors.map((error) => ({
+          property: error.property,
+          message: error.constraints[Object.keys(error.constraints)[0]],
+        }));
+        return new BadRequestException(result);
+      },
     }),
   );
   config.update({
